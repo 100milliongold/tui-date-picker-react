157fa7adf220afa4fd0b3e1e389886a0056f9262
xiilab <je.kim@xiilab.com>
2020-05-13 18:03:57 +0900

    최초

--- example/src/App.js
+++ example/src/App.js
@@ -4,7 +4,7 @@ import { ExampleComponent } from 'tui.date-picker-react'
 import 'tui.date-picker-react/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <ExampleComponent date={new Date()} format='yyyy/MM/dd HH:mm' timePicker={true} />
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,46 @@
 import * as React from 'react'
-import styles from './styles.module.css'
+import { useRef, useEffect, useState } from 'react'
+// eslint-disable-next-line no-unused-vars
+import DatePicker, { DatePickerOptions } from 'tui-date-picker'
+import 'tui-date-picker/dist/tui-date-picker.css'
 
-interface Props {
-  text: string
+interface Props extends DatePickerOptions {
+  date: Date
+  format: string
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const ExampleComponent = (props: Props) => {
+  const { date, format, showAlways, timePicker } = props
+  const tuiWrapperRef = useRef(null)
+  const tuiInputRef = useRef(null)
+  const [tui, setTui] = useState<DatePicker>()
+  useEffect(() => {
+    if (tui === undefined) {
+      setTui(
+        new DatePicker(tuiWrapperRef.current || '#wrapper', {
+          date: date,
+          input: {
+            element: tuiInputRef.current || '#datepicker-input',
+            format: format || 'yyyy-MM-dd'
+          },
+          showAlways: showAlways || false,
+          timePicker: timePicker || false
+        })
+      )
+    }
+    return () => {
+      if (tui !== undefined) {
+        tui.destroy()
+      }
+    }
+  })
+  return (
+    <div>
+      <div className='tui-datepicker-input tui-datetime-input tui-has-focus'>
+        <input type='text' ref={tuiInputRef} id='datepicker-input' />
+        <span className='tui-ico-date' />
+      </div>
+      <div id='wrapper' ref={tuiWrapperRef} />
+    </div>
+  )
 }
